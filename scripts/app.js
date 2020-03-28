7bab49b25eec8a681c088eca7ab0d933825e9e7e
tjgillweb <tjgillweb@gmail.com>
2020-03-28 23:16:48 +0530

    Refactor code using ternary operator

--- scripts/app.js
+++ scripts/app.js
@@ -29,12 +29,16 @@ const updateUI = (data) => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
 
-    let timeSrc = null;
-    if (weather.IsDayTime) {
-        timeSrc = "img/day.svg";
-    } else {
-        timeSrc = "img/night.svg";
-    }
+    // let timeSrc = null;
+    // if (weather.IsDayTime) {
+    //     timeSrc = "img/day.svg";
+    // } else {
+    //     timeSrc = "img/night.svg";
+    // }
+
+    //refactor using ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
     time.setAttribute("src", timeSrc);
 
     //remove d-none class
